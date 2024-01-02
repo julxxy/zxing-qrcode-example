6ab0e91dd3571cff1a8c5e07e65e620fc6cefee9
julian <julxxy@outlook.com>
2024-01-02 15:55:51 +0800

    feature:更新描述

---
+++ src/test/java/com/example/git/GitCommitHistory.java
@@ -0,0 +1,14 @@
+package com.example.git;
+
+/**
+ * Enter the description of this class here
+ *
+ * @author weasley
+ * @version 1.0.0
+ */
+public class GitCommitHistory {
+
+    void test1() {
+        System.out.println("test1");
+    }
+}
